eabca65c6677099f92f08193999b6d591f8d75b6
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2024-10-13 22:50:26 +0200

    renamed ex1 to when

--- sed/when.bash
+++ sed/when.bash
@@ -3,11 +3,11 @@
 #Usare grep per selezionare tutte le linee che contengono la parola “when” oppure “When”
 #Usare sed per selezionare tutte le linee che contengono la parola “when” oppure “When”
 
-#grep -i "when" when.txt;
+grep -i "when" when.txt;
 grep "[Ww]hen" when.txt;
 egrep "(w|W)hen" when.txt;
 
 echo "";
 
-#sed -n -e "/when/p" -e "/When/p" when.txt;
+sed -n -e "/when/p" -e "/When/p" when.txt;
 sed -n -e "/[Ww]hen/p" when.txt;
