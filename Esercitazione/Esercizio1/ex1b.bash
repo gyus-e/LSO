a27f4079b9054acea5e205f452dec7dd7a53c4fa
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2024-10-14 18:05:18 +0200

    fixes and other solutions

--- Esercitazione/Esercizio1/ex1b.bash
+++ Esercitazione/Esercizio1/ex1b.bash
@@ -2,4 +2,8 @@
 
 #(b) Elencare i nomi dei files regolari appartenenti all’utente "utente1" oppure al gruppo "staff"
 
-ls -l | egrep "^-.*" | awk '($3=="utente1" || $4=="staff") { print $9 }'
+ls -l | egrep "^-" | awk '($3=="utente1" || $4=="staff") { print $9 }';
+
+ls -l | awk '"^-" ($3=="utente1" || $4=="staff") { print $9 }';
+
+ls -l | awk ' ($1=="^-.*") ($3=="utente1" || $4=="staff") { print $9 }';
--- Esercitazione/Esercizio1/ex1d.bash
+++ Esercitazione/Esercizio1/ex1d.bash
@@ -2,4 +2,5 @@
 
 #(d) Elencare i nomi dei files modificati tra il 2006 e il 2008
 
-ls -l --time-style="+%Y-%m-%d" | awk ' { print $6 " " $7 } ' | egrep "^(2006|2007|2008)-([0-1][0-9])-([0-3][0-9])";
+ls -l --time-style="+%Y-%m-%d" | awk ' { print $6 " " $7 } ' | egrep "^(2006|2007|2008)";
+
