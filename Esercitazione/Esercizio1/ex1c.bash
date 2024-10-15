26e88dd8507de1682ef43b865a952b47cc9fff77
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2024-10-15 09:07:20 +0200

    optimization: use one awk call

--- Esercitazione/Esercizio1/ex1c.bash
+++ Esercitazione/Esercizio1/ex1c.bash
@@ -2,4 +2,4 @@
 
 #(c) Stampare la dimensione totale occupata dai files regolari appartenenti al gruppo staff
 
-ls -l | awk ' $4=="staff" { print $5 }' | awk ' { s+=$1 } END {print s} '
+ls -l | awk ' $4=="staff" { s+=$5 } END {print s} '
