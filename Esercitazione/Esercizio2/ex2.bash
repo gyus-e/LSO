efd6904af864857799e3e0e0975c04d95387d42f
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2024-10-18 12:41:42 +0200

    added part 2 and refactored

--- Esercitazione/Esercizio2/ex2.bash
+++ Esercitazione/Esercizio2/ex2.bash
@@ -1,38 +1,134 @@
 #!/bin/bash
 
-dir=$1
-
-if [ ! -d "$dir" ]
-then 
-    echo "specified directory does not exist"; 
-    exit 1;
-fi
-
-groupNames=$(ls -l $dir | awk ' {print $4} ' | sort -u);
-
-for groupName in $groupNames
-do  
-    if [ ! -d "output/$groupName" ]
-    then
-        mkdir output/$groupName
+checkIfDirectoryExists () {
+    if [ ! -d "$1" ]
+    then 
+        echo "specified directory does not exist"; 
+        exit 1;
     fi
+}
 
-    groupUsers=$(ls -l $dir | awk -v groupName=$groupName '$4==groupName {print $3}' | sort -u);
-    for userName in $groupUsers
-    do 
-        if [ ! -d "output/$groupName/$userName" ]
-        then
-            mkdir output/$groupName/$userName
-        fi
+makeDirectory () {
+    dirName=$1;
+    if [ ! -d "$dirName" ]
+    then
+        mkdir $dirName;
+    fi
+}
 
-        fileNames=$(ls -l $dir | awk -v userName=$userName -v groupName=$groupName '$3==userName && $4==groupName {print $9}' | sort -u);
-        for fileName in $fileNames
-        do
-            #si usa grep per prendere solo i caratteri stampabili eliminando i caratteri speciali
-            oldPath=$(echo "$dir/$fileName" | grep [[:print:]]); 
-            newPath=$(echo "output/$groupName/$userName/$fileName" | grep [[:print:]]);
+countDirectoryEntries () {
+    dir=$1;
+    directoryEntries=$(ls "$dir" | grep -c ".*");
+    echo $directoryEntries;
+}
 
-            cp "$oldPath" "$newPath"; 
+checkDirectoryIsEmpty () {
+    dir=$1;
+    directoryEntries=$(countDirectoryEntries $dir);
+    if [ $directoryEntries -eq 0 ]
+    then 
+        echo true;
+    fi
+}
+
+getGroupsFromDirectory () {
+    dir=$1;
+
+    groupNames=$(ls -l $dir | awk ' {print $4} ' | sort -u);
+    echo $groupNames;
+}
+
+getUsersInGroupFromDirectory () {
+    groupName=$1;
+    dir=$2;
+
+    userNames=$(ls -l $dir | awk -v groupName=$groupName '$4==groupName {print $3}' | sort -u);
+    echo $userNames;
+}
+
+getFilesOfUserInGroupFromDirectory () {
+    userName=$1;
+    groupName=$2;
+    dir=$3;
+
+    fileNames=$(ls -l $dir | awk -v userName=$userName -v groupName=$groupName '$3==userName && $4==groupName {print $9}' | sort -u);
+    echo $fileNames;
+}
+
+cleanPath () {
+    path=$1;
+    echo "$path" | grep [[:print:]];
+}
+
+part1 () {
+    dir=$1;
+
+    groupNames=$(getGroupsFromDirectory $dir);
+    for groupName in $groupNames
+    do
+        makeDirectory "output/$groupName";
+
+        userNames=$(getUsersInGroupFromDirectory $groupName $dir);
+        for userName in $userNames
+        do 
+            makeDirectory "output/$groupName/$userName";
+            
+            fileNames=$(getFilesOfUserInGroupFromDirectory $userName $groupName $dir);
+            for fileName in $fileNames
+            do
+                sorucePath=$(cleanPath "$dir/$fileName");
+                targetPath=$(cleanPath "output/$groupName/$userName/$fileName");
+                cp "$sorucePath" "$targetPath"; 
+            done
         done
     done
-done
+}
+
+part2 () {
+    dir=$1;
+
+    groupNames=$(getGroupsFromDirectory $dir);
+    for groupName in $groupNames 
+    do 
+        howManyInGroupDirectory=$(countDirectoryEntries "output/$groupName");
+
+        userNames=$(getUsersInGroupFromDirectory $groupName $dir);
+        for userName in $userNames 
+        do
+            howManyInUserDirectory=$(countDirectoryEntries "output/$groupName/$userName");
+
+            fileNames=$(getFilesOfUserInGroupFromDirectory $userName $groupName $dir);
+            for fileName in $fileNames 
+            do 
+                echo "Delete $groupName/$userName/$fileName? [Y/N]";
+                read fileIsToBeDeleted;
+                if [ $fileIsToBeDeleted == "Y" ] || [ $fileIsToBeDeleted == "y" ]
+                then
+                    rm "$(cleanPath "output/$groupName/$userName/$fileName")";
+                    ((howManyInUserDirectory--));
+                fi
+
+                if  [ $howManyInUserDirectory -eq 0 ]
+                then 
+                    rmdir "$(cleanPath "output/$groupName/$userName")";
+                    ((howManyInGroupDirectory--));
+                fi
+            done
+        done
+            
+        if [ $howManyInGroupDirectory -eq 0 ]
+        then 
+            rmdir "$(cleanPath "output/$groupName")";
+        fi
+    done
+}
+
+main () {
+    dir=$1
+    checkIfDirectoryExists "$dir";
+
+    part1 $dir;
+    part2 $dir;
+}
+
+main $@;
